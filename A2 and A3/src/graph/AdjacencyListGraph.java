5011a2cda6a3f63df2e17f142c625481c66024a5
harinder24 <hss022002@gmail.com>
2022-12-02 17:39:05 -0800

    add vertex and add edge done
    in add vertex it check v exists or not if nor it adds and in edge  it checks both v1 and v2 exist then check if  edge to v1 exist if not it adds

--- A2 and A3/src/graph/AdjacencyListGraph.java
+++ A2 and A3/src/graph/AdjacencyListGraph.java
@@ -1,4 +1,24 @@
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AdjacencyListGraph implements Graph {
-  // TODO: Implement this class
+
+  private Map<Vertex, List<Vertex>> adjacencyMapList = new HashMap<>();
+
+  @Override
+  public void addVertex(Vertex v) {
+    // only add the vertex if it does not exist
+    if (!adjacencyMapList.containsKey(v)) {
+      adjacencyMapList.put(v, new ArrayList<>());
+    }
+  }
+
+  @Override
+  public void addEdge(Vertex v1, Vertex v2) {
+    // only add an edge if the v1 and v2 vertex exist and the edge has not been
+    // added
+    if (adjacencyMapList.containsKey(v1) && adjacencyMapList.containsKey(v2) && !edgeExists(v1, v2)) {
+      adjacencyMapList.get(v1).add(v2);
+    }
+  }
 }
